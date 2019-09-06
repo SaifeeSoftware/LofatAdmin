dc051575d264f3a53184140203666ddfbb06f58d
Akhilesh M <akhileshm@saifeesoftware.com>
2019-09-06 15:54:14 +0530

    validation commit

--- js/validate.js
+++ js/validate.js
@@ -1,104 +1,104 @@
 
-function printError(elemId, hintMsg) {
-    document.getElementById(elemId).innerHTML = hintMsg;
-}
+// function printError(elemId, hintMsg) {
+//     document.getElementById(elemId).innerHTML = hintMsg;
+// }
 
-function validate() {
+// function validate() {
     
-    var fchoose= document.adminForm.fchoose.value;
-    var nameplan=document.adminForm.nameplan.value;
-    var selectedplanname=document.adminForm.selectedplanname.value;
+//     var fchoose= document.adminForm.fchoose.value;
+//     var nameplan=document.adminForm.nameplan.value;
+//     var selectedplanname=document.adminForm.selectedplanname.value;
 
-    var namemeal=document.adminForm.namemeal.value;
-     var descmeal=document.adminForm.descmeal.value;
+//     var namemeal=document.adminForm.namemeal.value;
+//      var descmeal=document.adminForm.descmeal.value;
 
 
-    var cust_name=document.adminForm.cust_name.value;
-     var cust_desc=document.adminForm.cust_desc.value;
-    var fchooseprofile=document.adminForm.fchooseprofile.value;
+//     var cust_name=document.adminForm.cust_name.value;
+//      var cust_desc=document.adminForm.cust_desc.value;
+//     var fchooseprofile=document.adminForm.fchooseprofile.value;
 
- var fileErr=addplanErr=selectErr=mealErr=mealdescErr=custnameErr=custdesc=ErrfilecErr=true;
- if(fchoose=="")
-{
-printError("fileErr","Please Select a file");
-}
-else
- {
-printError("fileErr","");
-fileErr=false;
-}
+//  var fileErr=addplanErr=selectErr=mealErr=mealdescErr=custnameErr=custdesc=ErrfilecErr=true;
+//  if(fchoose=="")
+// {
+// printError("fileErr","Please Select a file");
+// }
+// else
+//  {
+// printError("fileErr","");
+// fileErr=false;
+// }
 
-if(nameplan=="")
-{
-printError("addplanErr","Please add plan");
-}
-else
- {
-printError("addplanErr","");
-addplanErr=false;
-}
+// if(nameplan=="")
+// {
+// printError("addplanErr","Please add plan");
+// }
+// else
+//  {
+// printError("addplanErr","");
+// addplanErr=false;
+// }
 
-if(selectedplanname=="Please select")
-{
-printError("selectErr","Please select");
-}
-else
- {
-printError("selectErr","");
-selectErr=false;
-}
+// if(selectedplanname=="Please select")
+// {
+// printError("selectErr","Please select");
+// }
+// else
+//  {
+// printError("selectErr","");
+// selectErr=false;
+// }
 
-if(namemeal=="")
-{
-printError("mealErr","Please fill");
-}
-else
- {
-printError("mealErr","");
-mealErr=false;
-}
+// if(namemeal=="")
+// {
+// printError("mealErr","Please fill");
+// }
+// else
+//  {
+// printError("mealErr","");
+// mealErr=false;
+// }
 
-if(descmeal=="")
-{
-printError("mealdescErr","Please fill");
-}
-else
- {
-printError("mealdescErr","");
-mealdescErr=false;
-}
+// if(descmeal=="")
+// {
+// printError("mealdescErr","Please fill");
+// }
+// else
+//  {
+// printError("mealdescErr","");
+// mealdescErr=false;
+// }
 
-if(cust_name=="")
-{
-printError("custnameErr","Please fill");
-}
-else
- {
-printError("custnameErr","");
-custnameErr=false;
-}
+// if(cust_name=="")
+// {
+// printError("custnameErr","Please fill");
+// }
+// else
+//  {
+// printError("custnameErr","");
+// custnameErr=false;
+// }
 
-if(cust_desc=="")
-{
-printError("custdesc","Please fill");
-}
-else
- {
-printError("custdesc","");
-custdesc=false;
-}
+// if(cust_desc=="")
+// {
+// printError("custdesc","Please fill");
+// }
+// else
+//  {
+// printError("custdesc","");
+// custdesc=false;
+// }
 
-if(fchooseprofile=="")
-{
-printError("ErrfilecErr","Please fill");
-}
-else
- {
-printError("ErrfilecErr","");
-ErrfilecErr=false;
-}
+// if(fchooseprofile=="")
+// {
+// printError("ErrfilecErr","Please fill");
+// }
+// else
+//  {
+// printError("ErrfilecErr","");
+// ErrfilecErr=false;
+// }
 
-};
+// };
 // function bannerImage() {
 //     var x = document.forms["bannerForm"]["fchoose"].value;
 //     if (x == "") {
@@ -155,17 +155,41 @@ ErrfilecErr=false;
 //     }
 //   }
 
- function addMeal(){
-    var s1=document.getElementById('ddSelectPlan').value;
-    if(s1=="Please select"){
-        alert("Please select any one from the list ");
-     return false;
+//  function addMeal(){
+//     var s1=document.getElementById('ddSelectPlan').value;
+//    if(s1=="Please select"){
+//          alert("Please select any one from the list ");
+//     return false;
+//     }
+//    return true;
+//   }
+
+
+
+
+
+
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+    
+    var forms = document.getElementsByClassName('needs-validation');
+    
+    var validation = Array.prototype.filter.call(forms, function(form) {
+    form.addEventListener('submit', function(event) {
+    if (form.checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
     }
-   return true;
-   }
+    form.classList.add('was-validated');
+    }, false);
+    });
+    }, false);
+    })();
 
-  
+    
 
+    
   $('#sliderinput').on('change',function(){
     //get the file name
     
@@ -182,29 +206,7 @@ $('#custprofileinput').on('change',function(){
 })
 
 
-  $(document).ready(function() {
-    $('#bannerForm').bootstrapValidator({
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-            fchoose: {
-                validators: {
-                    file: {
-                        extension: 'jpeg,png',
-                        type: 'image/jpeg,image/png',
-                        maxSize: 2048 * 1024,
-                        message: 'The selected file is not valid'
-                    }
-                }
-            }
-        }
-    });
-});
-
-
+  
 
 
 var data = [{
