ac3fb63bd027daba1b3aac008c1e30b204a2d3c0
Akhilesh M <akhileshm@saifeesoftware.com>
2019-09-14 13:07:16 +0530

    Validation Commit

--- js/validate.js
+++ js/validate.js
@@ -1,204 +1,120 @@
 
-// function printError(elemId, hintMsg) {
-//     document.getElementById(elemId).innerHTML = hintMsg;
-// }
+function pass_val(pass1)
+{
+    var regPass= /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d][A-Za-z\d!@#$%^&*()_+]{7,9}$/;
+    if(regPass.test(pass1) == false)
+    {
+        document.getElementById("status1").innerHTML    = "<span class='warning'>Please enter you password between 7-9 character atleast one number, alphabet, special character</span>";
 
-// function validate() {
-    
-//     var fchoose= document.adminForm.fchoose.value;
-//     var nameplan=document.adminForm.nameplan.value;
-//     var selectedplanname=document.adminForm.selectedplanname.value;
-
-//     var namemeal=document.adminForm.namemeal.value;
-//      var descmeal=document.adminForm.descmeal.value;
-
-
-//     var cust_name=document.adminForm.cust_name.value;
-//      var cust_desc=document.adminForm.cust_desc.value;
-//     var fchooseprofile=document.adminForm.fchooseprofile.value;
-
-//  var fileErr=addplanErr=selectErr=mealErr=mealdescErr=custnameErr=custdesc=ErrfilecErr=true;
-//  if(fchoose=="")
-// {
-// printError("fileErr","Please Select a file");
-// }
-// else
-//  {
-// printError("fileErr","");
-// fileErr=false;
-// }
-
-// if(nameplan=="")
-// {
-// printError("addplanErr","Please add plan");
-// }
-// else
-//  {
-// printError("addplanErr","");
-// addplanErr=false;
-// }
-
-// if(selectedplanname=="Please select")
-// {
-// printError("selectErr","Please select");
-// }
-// else
-//  {
-// printError("selectErr","");
-// selectErr=false;
-// }
-
-// if(namemeal=="")
-// {
-// printError("mealErr","Please fill");
-// }
-// else
-//  {
-// printError("mealErr","");
-// mealErr=false;
-// }
-
-// if(descmeal=="")
-// {
-// printError("mealdescErr","Please fill");
-// }
-// else
-//  {
-// printError("mealdescErr","");
-// mealdescErr=false;
-// }
-
-// if(cust_name=="")
-// {
-// printError("custnameErr","Please fill");
-// }
-// else
-//  {
-// printError("custnameErr","");
-// custnameErr=false;
-// }
-
-// if(cust_desc=="")
-// {
-// printError("custdesc","Please fill");
-// }
-// else
-//  {
-// printError("custdesc","");
-// custdesc=false;
-// }
-
-// if(fchooseprofile=="")
-// {
-// printError("ErrfilecErr","Please fill");
-// }
-// else
-//  {
-// printError("ErrfilecErr","");
-// ErrfilecErr=false;
-// }
-
-// };
-// function bannerImage() {
-//     var x = document.forms["bannerForm"]["fchoose"].value;
-//     if (x == "") {
-//         alert("Please provide an input");
-//       return false;
-//     }
-//     return true;
-//   }
-  
-
-
-//   function newPlan() {
-//     var x = document.forms["planForm"]["nameplan"].value;
-//     if (x == "") {
-     
-//       return false;
-//     }
-//     return true;
-//   }
-//  function newMeal(){
-//                 var x = document.forms["planForm"]["nameplan"].value;
-//                     if (x == "") {
-//                      alert("Please provide an input");
-//                     return false;
-//                     }
-//       return true;
-//     }
-
-//    function addMeal(addMealForm)
-//    {
-//          var ddl = document.getElementById("ddSelectPlan");
-//         var selectedValue = ddl.options[ddl.selectedIndex].value;
-//         if (selectedValue == "Please select")
-//             {
-//                 alert("Please select anyone from Select list");
-//                 return false;
-//              } 
-//       return true;
-//     }
-  
-//   function addMeal() {
-//     var x = document.forms["addMealForm"]["selectedplanname","namemeal","descmeal"].value;
-//     if (x == "") {
-      
-//       return false;
-//     }
-//   }
-
-//   function custStories() {
-//     var x = document.forms["customerForm"]["cust_name","cust_desc","f_choose"].value;
-//     if (x == "") {
-      
-//       return false;
-//     }
-//   }
-
-//  function addMeal(){
-//     var s1=document.getElementById('ddSelectPlan').value;
-//    if(s1=="Please select"){
-//          alert("Please select any one from the list ");
-//     return false;
-//     }
-//    return true;
-//   }
-
-
-
-
-
-
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-    
-    var forms = document.getElementsByClassName('needs-validation');
-    
-    var validation = Array.prototype.filter.call(forms, function(form) {
-    form.addEventListener('submit', function(event) {
-    if (form.checkValidity() === false) {
-    event.preventDefault();
-    event.stopPropagation();
     }
-    form.classList.add('was-validated');
-    }, false);
-    });
-    }, false);
-    })();
+    else
+    {
+        document.getElementById("status1").innerHTML	= "<span class='valid'>Thanks for entering password</span>";	  
+    }
 
-    
 
-    
-  $('#sliderinput').on('change',function(){
+}
+function checkPass()
+{
+    //Store the password field objects into variables ...
+    var pass1 = document.getElementById('pass1');
+    var pass2 = document.getElementById('pass2');
+    //Store the Confimation Message Object ...
+    var message = document.getElementById('confirmMessage');
+    //Set the colors we will be using ...
+    var goodColor = "#66cc66";
+    var badColor = "#ff6666";
+    //Compare the values in the password field 
+    //and the confirmation field
+    if(pass1.value == pass2.value){
+        //The passwords match. 
+        //Set the color to the good color and inform
+        //the user that they have entered the correct password 
+        pass2.style.backgroundColor = goodColor;
+        message.style.color = goodColor;
+        message.innerHTML = "Passwords Match"
+        
+    }else{
+        //The passwords do not match.
+        //Set the color to the bad color and
+        //notify the user.
+        pass2.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = "Passwords Do Not Match!"
+    }
+} 
+
+
+
+
+// validate email
+function email_validate(email)
+{
+var regMail = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,3})$/;
+
+    if(regMail.test(email) == false)
+    {
+    document.getElementById("status").innerHTML    = "<span class='warning'>Email address is not valid yet.</span>";
+    }
+    else
+    {
+    document.getElementById("status").innerHTML	= "<span class='valid'>Thanks, you have entered a valid Email address!</span>";	
+    }
+}
+
+// validates text only
+function Validate(txt) {
+    txt.value = txt.value.replace(/[^a-zA-Z-'\n\r.]+/g, '');
+}
+
+
+
+
+
+
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+
+        var forms = document.getElementsByClassName('needs-validation');
+
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                } else {
+                    debugger;
+                    switch (form.id) {
+                        case "addNewPlan":
+                            addPlan();
+                            break;
+                        case "addPlanMeadDesc":
+                            addMeal();
+                            break;
+                        
+
+
+                    }
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
+
+
+
+$('#sliderinput').on('change', function () {
     //get the file name
-    
+
 
     var fileName = $(this).val();
     //replace the "Choose a file" label
     $(this).next('.custom-file-label').html(fileName);
 })
-$('#custprofileinput').on('change',function(){
+$('#custprofileinput').on('change', function () {
     //get the file name
     var fileName = $(this).val();
     //replace the "Choose a file" label
@@ -206,54 +122,122 @@ $('#custprofileinput').on('change',function(){
 })
 
 
-  
+    // THE JSON ARRAY.
+    
+        // THE JSON ARRAY.
+        var data = [
+{
+    "plan": "Wholesome Membership",
+    "meals": [{
+        "name": "Biryani",
+        "description": "Royal Biryani"
+    },
+    {
+        "name": "Pan Pizza",
+        "description": "Pizza with beans"
+    },
+    {
+        "name": "Mendu Vada",
+        "description": "Mendu vada with sambahar"
+    }
 
 
-var data = [{
-    "Wholesome": [{
-            "meal": "Biryani",
-            "description": "Royal Biryani"
-        },
-        {
-            "meal": "Pan Pizza",
-            "description": "Pizza with beans"
-        },
-        {
-            "meal": "Mendu Vada",
-            "description": "Mendu vada with sambahar"
-        }
     ]
 },
 {
-    "Essential": [{
-            "meal": "Burger",
-            "description": "sada burger"
-        },
-        {
-            "meal": "Chole Bhature",
-            "description": "Bhature with onion"
-        },
-        {
-            "meal": "Samosa",
-            "description": "Samosa with sweet chutney"
-        }
+    "plan": "Essential Membership",
+    "meals": [{
+        "name": "Burger",
+        "description": "sada burger"
+    },
+    {
+        "name": "Chole Bhature",
+        "description": "Bhature with onion"
+    },
+    {
+        "name": "Samosa",
+        "description": "Samosa with sweet chutney"
+    }
     ]
 },
 {
-    "Vegetarian": [{
-            "meal": "green salad",
-            "description": "Carrot, Radish"
-        },
-        {
-            "meal": "Idli",
-            "description": "Chutney , Sambhar"
-        },
-        {
-            "meal": "Paneer",
-            "description": "Palak, Paneer"
-        }
+    "plan": "Vegetarian Membership",
+    "meals": [{
+        "name": "green salad",
+        "description": "Carrot, Radish"
+    },
+    {
+        "name": "Idli",
+        "description": "Chutney , Sambhar"
+    },
+    {
+        "name": "Paneer",
+        "description": "Palak, Paneer"
+    }
     ]
 }
-]
+
+];
+function populateSelect()
+    {
+        var ele = document.getElementById('ddSelectPlan');
+        for (var i = 0; i < data.length; i++) 
+        {
+            // POPULATE SELECT ELEMENT WITH JSON.
+            ele.innerHTML = ele.innerHTML +
+                '<option value>'+ data[i]['plan']+'</option>';
+               
+        }
+    }
+
+
+    $(document).ready(function()
+    {
+        populateSelect();
+    });
+
+var plantemplate = {
+    "plan": "",
+    "meals": []
+};
+var mealtemplate = {
+    "name": "",
+    "description": ""
+}
+function addPlan() {
+    var plan = $("#plan").val();
+    
+    if (plan !== "") {
+        plantemplate.plan = plan;
+        data.push(plantemplate);
+
+        console.log(data);
+    }
+}
+
+function addMeal() {
+    var meal = $("#meal").val();
+    var mealdesc = $("#mealdesc").val();
+    
+    if (meal !== "") {
+        mealtemplate.mealdesc = mealdesc;
+        mealtemplate.meal = meal;
+        var addSelectPlan = $("#ddSelectPlan option:selected").text();
+        $.each(data, function (i, v) {            
+            if(v.plan === addSelectPlan){
+                
+                v.meals.push(mealtemplate)
+                return;
+            }
+        });
+    }
+
+}
+
+
+
+
+
+
 
 
