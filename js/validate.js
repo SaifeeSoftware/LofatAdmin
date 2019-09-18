5f57c61dd5935cdcfa0bd33408c8063b23a03965
Akhilesh M <akhileshm@saifeesoftware.com>
2019-09-18 11:48:40 +0530

    string json commit

--- js/validate.js
+++ js/validate.js
@@ -361,35 +361,35 @@ function addCustomerdata()
                      }
 }
 
- function readFile() {
-    if (this.files && this.files[0]) {
+//  function readFile() {
+//     if (this.files && this.files[0]) {
       
-       var FR= new FileReader();
+//        var FR= new FileReader();
       
-       FR.addEventListener("load", function(e) {
-        document.getElementById("imageCustStories").src       = e.target.result;
-         document.getElementById("b64").innerHTML = e.target.result;
-      }); 
+//        FR.addEventListener("load", function(e) {
+//         document.getElementById("imageCustStories").src       = e.target.result;
+//          document.getElementById("b64").innerHTML = e.target.result;
+//       }); 
       
-       FR.readAsDataURL( this.files[0] );
-    }
-  }
+//        FR.readAsDataURL( this.files[0] );
+//     }
+//   }
   
-  document.getElementById("custprofileinput").addEventListener("change", readFile);
+//   document.getElementById("custprofileinput").addEventListener("change", readFile);
 
 
-// File.prototype.convertToBase64 = function(callback){
-//     var reader = new FileReader();
-//     reader.onloadend = function (e) {
-//         callback(e.target.result, e.target.error);
-//     };   
-//     reader.readAsDataURL(this);
-// };
+ File.prototype.convertToBase64 = function(callback){
+    var reader = new FileReader();
+     reader.onloadend = function (e) {
+         callback(e.target.result, e.target.error);
+    };   
+    reader.readAsDataURL(this);
+};
 
-// $("#custprofileinput").on('change',function(){
-// var selectedFile = this.files[0];
-// selectedFile.convertToBase64(function(base64){
-//    alert(base64);
-// }) 
-// });
+ $("#custprofileinput").on('change',function(){
+ var selectedFile = this.files[0];
+ selectedFile.convertToBase64(function(base64){
+    // alert(base64);
+ }) 
+ });
 
