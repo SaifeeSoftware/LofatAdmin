50befd66c0cd1d89ef46abc3bd8fa996f3f6342e
Akhilesh M <akhileshm@saifeesoftware.com>
2019-09-24 18:19:01 +0530

    xhr request commit

--- js/common.js
+++ js/common.js
@@ -1,23 +1,50 @@
 function sendRequest(endpoint, jsondata, succFunc, errorFunc, requestType) {
-    var apiurl = "http://localhost:8080/api/"
+	var apiurl = "http://localhost:8080/api/";
 
-    $.ajax({
-        url: apiurl + endpoint,
 
-        data: JSON.stringify(jsondata),
+	$.ajax({
+		url: apiurl + endpoint,
 
-        contentType: "application/json; charset=utf-8",
+		data: JSON.stringify(jsondata),
 
-        type: requestType,
+		contentType: "application/json; charset=utf-8",
 
-        dataType: "json",
+		type: requestType,
 
-        success: function(result) {
-            succFunc(result);
-        },
-        error: function(err) {
-            errorFunc(err);
-        }
 
-    });
+		dataType: "json",
+
+		success: function(result) {
+			succFunc(result);
+		},
+		error: function(err) {
+			errorFunc(err);
+		}
+
+	});
+}
+
+function sendXhrHttpRequest(endpoint, data, succFunct, errorFunct) {
+	var apiurl = "http://localhost:8080/api/";
+
+
+	var xhr = new XMLHttpRequest();
+	xhr.withCredentials = true;
+	url: apiurl + endpoint;
+
+	xhr.addEventListener("readystatechange", function() {
+		if (this.readyState === 4) {
+			success: function(this, responseText) {
+				succFunct(this, responseText);
+			}
+			console.log(this.responseText);
+		}
+
+	});
+
+	xhr.open("POST", url);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.setRequestHeader("Accept", "application/json");
+
+	xhr.send(data);
 }
